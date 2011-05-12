0922a290ffc63e66875ea317ea7c94787b013f48
David Zuelke <david.zuelke@bitextender.com>
2011-05-12 14:20:55 +0200

    remove eol anchor from apache log regex, that way lines with referrers and/or user agents can be parsed too (although those two fields won't show up for now, can't be arsed to fix this properly at the minute

--- lib/Hadoophp/MapReduce/Util.php
+++ lib/Hadoophp/MapReduce/Util.php
@@ -5,7 +5,7 @@ namespace Hadoophp\MapReduce;
 class Util {
 	
 	public static function parseApacheLogLine($line) {
-		if(preg_match('/^(?P<ip>[0-9a-fA-F.:]+)\s(?P<identd>\S+)\s(?P<authuser>\S+)\s\[(?P<timestamp>[^\]]+)\]\s"(?P<request>(?P<request_method>[A-Z]+)\s(?P<request_uri>\S+)\s(?P<request_protocol>\S+))"\s(?P<status>\d+)\s(?P<length>(\d+|-))$/', $line, $matches)) {
+		if(preg_match('/^(?P<ip>[0-9a-fA-F.:]+)\s(?P<identd>\S+)\s(?P<authuser>\S+)\s\[(?P<timestamp>[^\]]+)\]\s"(?P<request>(?P<request_method>[A-Z]+)\s(?P<request_uri>\S+)\s(?P<request_protocol>\S+))"\s(?P<status>\d+)\s(?P<length>(\d+|-))/', $line, $matches)) {
 			$matches['datetime'] = new \DateTime($matches['timestamp']);
 			if($matches['length'] == '-') {
 				$matches['length'] = '0';
